c6ff214972952a18af3ac96b4194dca8d187bad9
Hagen Paul Pfeifer <hagen@jauu.net>
2017-01-03 20:08:04 +0100

    implement proper configuration handling

--- dmpr.py
+++ dmpr.py
@@ -1,15 +1,59 @@
+import uuid
 
+class ConfigurationException(Exception): pass
+
+class DMPRConfigDefaults(object):
+    RTN_MSG_INTERVAL = 30
+    RTN_MSG_INTERVAL_JITTER = int(RTN_MSG_INTERVAL / 4)
+    # plus 1 here is a pratical "hack", just to make sure
+    # it is always removed later. Normally this is not required
+    # but for simulation environment with a seconds granularity
+    # things are different.
+    RTN_MSG_HOLD_TIME = RTN_MSG_INTERVAL * 3 + 1
+
+    @staticmethod
+    def get(name):
+        return getattr(self, name)
+
+
+def id_generator:
+    return str(uuid.uuid1())
 
 class DMPR(object):
 
     def __init__(self, log):
+        self.conf = None
         self.log = log
 
-    def conf(self, conf):
-        self.conf = conf
+    def register_configuration(self, conf):
+        """ register and setup configuration. Raise
+            an error when values are wrongly configured """
+        assert(conf)
+        assert isinstance(conf, dict)
+        self.configuration = conf
+        self.process_conf()
 
     def process_conf(self):
-        pass
+        """ convert external python dict configuration
+            into internal configuration and check values """
+        assert(self.configuration)
+        self.conf = {}
+        cmd = "RTN_MSG_INTERVAL"
+        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        cmd = "RTN_MSG_INTERVAL_JITTER"
+        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        cmd = "RTN_MSG_HOLD_TIME"
+        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        if "id" not in self.configuration:
+            msg = "configuration contains no id! A id must be unique, it can be
+                   randomly generated but for better performance and debugging
+                   capabilities this generated ID should be saved permanently
+                   (e.g. at a local file) to survive daemon restarts"
+            raise ConfigurationException(msg)
+        if not isinstance(self.conf[cmd], str):
+            msg = "id must be a string!"
+            raise ConfigurationException(msg)
+        
 
     def stop(self):
         self.log("stop DMPR core")
