93e0e4008140bb286c3448f520df0a87383fd6de
Hagen Paul Pfeifer <hagen@jauu.net>
2017-01-04 17:16:06 +0100

    implement configuration parsing

--- dmpr.py
+++ dmpr.py
@@ -1,15 +1,30 @@
 import uuid
 
+# example configuration for DMPR daemon
+exa_conf = """
+    "id" : "ace80ef4-d284-11e6-bf26-cec0c932ce01",
+    "rtn_msg_interval" : "30",
+    "rtn_msg_interval_jitter" : "7",
+    "rtn_msg_hold_time" : "90",
+    "interfaces" : [
+      {"name" : "wlan0",  "link-characteristics" : { "bandwidth" : "100000", "loss" : "0"  } },
+      {"name" : "tetra0", "link-characteristics" : { "bandwidth" : "10000",  "loss" : "0"  } }
+    ],
+    "networks" : [
+       { "proto": "v4", "prefix" : "192.168.1.0", "prefix_len" : "24" },
+       { "proto": "v4", "prefix" : "192.168.2.0", "prefix_len" : "24" },
+       { "proto": "v4", "prefix" : "10.10.0.0",   "prefix_len" : "16" },
+       { "proto": "v6", "prefix" : "fdcb:523:1111::", "prefix_len" : "48" },
+       { "proto": "v6", "prefix" : "fd6a:6ad:b07f:ffff::", "prefix_len" : "64" }
+    }
+"""
+
 class ConfigurationException(Exception): pass
 
 class DMPRConfigDefaults(object):
-    RTN_MSG_INTERVAL = 30
-    RTN_MSG_INTERVAL_JITTER = int(RTN_MSG_INTERVAL / 4)
-    # plus 1 here is a pratical "hack", just to make sure
-    # it is always removed later. Normally this is not required
-    # but for simulation environment with a seconds granularity
-    # things are different.
-    RTN_MSG_HOLD_TIME = RTN_MSG_INTERVAL * 3 + 1
+    rtn_msg_interval = "30"
+    rtn_msg_interval_jitter = str(int(RTN_MSG_INTERVAL / 4))
+    rtn_msg_hold_time = str(RTN_MSG_INTERVAL * 3)
 
     # default bandwidth for a given interface in bytes/second
     # bytes/second enabled dmpr deployed in low bandwidth environments
@@ -18,15 +33,15 @@ class DMPRConfigDefaults(object):
     # Implementers SHOULD quantise values into a few classes to reduce the
     # DMPR routing packet size.
     # E.g. 1000, 5000, 10000, 100000, 1000000, 100000000, 100000000
-    LINK_CHARACTERISITCS_BANDWIDTH = 5000
+    LINK_CHARACTERISITCS_BANDWIDTH = "5000"
     # default loss is in percent for a given path
     # Implementers SHOULD quantise values into a few classes to reduce the
     # DMPR routing packet size.
     # e.g. 0, 5, 10, 20, 40, 80
-    LINK_CHARACTERISITCS_LOSS = 0
+    LINK_CHARACTERISITCS_LOSS = "0"
     # default link cost in a hypothetical currency, the higher the more valuable
     # e.g. wifi can be 0, LTE can be 100, satelite uplink can be 1000
-    LINK_CHARACTERISITCS_COST = 0
+    LINK_CHARACTERISITCS_COST = "0"
 
     @staticmethod
     def get(name):
@@ -42,26 +57,25 @@ class DMPR(object):
         self.conf = None
         self.log = log
 
-    def register_configuration(self, conf):
+    def register_configuration(self, configuration):
         """ register and setup configuration. Raise
             an error when values are wrongly configured """
-        assert(conf)
-        assert isinstance(conf, dict)
-        self.configuration = conf
-        self.process_conf()
+        assert(configuration)
+        assert isinstance(configuration, dict)
+        self.process_conf(configuration)
 
-    def process_conf(self):
+    def process_conf(self, configuration):
         """ convert external python dict configuration
             into internal configuration and check values """
-        assert(self.configuration)
+        assert(configuration)
         self.conf = {}
-        cmd = "RTN_MSG_INTERVAL"
-        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
-        cmd = "RTN_MSG_INTERVAL_JITTER"
-        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
-        cmd = "RTN_MSG_HOLD_TIME"
-        self.conf[cmd] = self.configuration.get(cmd, DMPRConfigDefaults.get(cmd))
-        if "id" not in self.configuration:
+        cmd = "rtn_msg_interval"
+        self.conf[cmd] = configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        cmd = "rtn_msg_interval_jitter"
+        self.conf[cmd] = configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        cmd = "rtn_msg_hold_time"
+        self.conf[cmd] = configuration.get(cmd, DMPRConfigDefaults.get(cmd))
+        if "id" not in configuration:
             msg = "configuration contains no id! A id must be unique, it can be
                    randomly generated but for better performance and debugging
                    capabilities this generated ID should be saved permanently
@@ -70,14 +84,17 @@ class DMPR(object):
         if not isinstance(self.conf[cmd], str):
             msg = "id must be a string!"
             raise ConfigurationException(msg)
-        if not "interfaces" in self.configuration:
+        if not "interfaces" in configuration:
             msg = "No interface configurated, need at least one"
             raise ConfigurationException(msg)
-        self.conf["interfaces"] = self.configuration["interfaces"]
+        self.conf["interfaces"] = configuration["interfaces"]
         if not isinstance(self.conf["interfaces"], list):
             msg = "interfaces must be a list!"
             raise ConfigurationException(msg)
-        for interface_name, interface_data in self.conf["interfaces"].items():
+        for interface_data in self.conf["interfaces"]:
+            if not isinstance(interface_data, dict):
+                msg = "interface entry must be dict: {}".format(interface_data)
+                raise ConfigurationException(msg)
             if "name" not in interface_data:
                 msg = "interfaces entry must contain at least a \"name\""
                 raise ConfigurationException(msg)
@@ -87,7 +104,37 @@ class DMPR(object):
                 interface_data["link-characteristics"] = dict()
                 interface_data["link-characteristics"]["bandwidth"] = DMPRConfigDefaults.LINK_CHARACTERISITCS_BANDWIDTH
                 interface_data["link-characteristics"]["loss"] = DMPRConfigDefaults.LINK_CHARACTERISITCS_LOSS
+        if "networks" in configuration:
+            if not isinstance(configuration["networks"], list):
+                msg = "networks must be a list!"
+                raise ConfigurationException(msg)
+            for network in configuration["networks"]:
+                if not isinstance(network, dict):
+                    msg = "interface entry must be dict: {}".format(network)
+                    raise ConfigurationException(msg)
+                if not "proto" in network:
+                    msg = "network must contain proto key: {}".format(network)
+                    raise ConfigurationException(msg)
+                if not "prefix" in network:
+                    msg = "network must contain prefix key: {}".format(network)
+                    raise ConfigurationException(msg)
+                if not "prefix_len" in network:
+                    msg = "network must contain prefix_len key: {}".format(network)
+                    raise ConfigurationException(msg)
+            # seens fine, save it as it is
+            self.conf["networks"] = configuration["networks"]
 
+    def tick(self, time):
+        """ this function is called every second, DMPR will
+            implement his own timer/timeout related functionality
+            based on this ticker. This is not the most efficient
+            way to implement timers but it is suitable to run in
+            a real and simulated environment where time is discret.
+            The argument time is a float value in seconds which should
+            not used in a absolute manner, depending on environment this
+            can be a unix timestamp or starting at 0 in simulation
+            environments """
+        pass
         
 
     def stop(self):
